4ede144aa3b11e28491d99043133df1f942cedbc
rocioparra <rparra@itba.edu.ar>
2017-05-27 15:25:40 -0300

    point y vector en varios tipos de dato
    ES UN BORRADOR

---
+++ vector.h
@@ -0,0 +1,25 @@
+#ifndef VECTOR_H
+#define	VECTOR_H
+
+#include <stdint.h>
+
+
+//tipos de datos punto (dos coordenadas cartesianas) y vector (dos puntos) con distintos tipos de datos
+
+//ESTO ES UN BORRADOR MALE NO ME RETES
+
+typedef struct { float x, y;} fpoint_t;
+
+typedef struct { fpoint_t start, end; } fvector_t;
+
+
+typedef struct { uint16_t x, y;} uipoint_t;
+
+typedef struct { uipoint_t start, end; } uivector_t;
+
+typedef struct { int16_t x, y;} ipoint_t;
+
+typedef struct { ipoint_t start, end; } ivector_t;
+
+
+#endif //VECTOR_H
