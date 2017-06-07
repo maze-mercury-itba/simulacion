46e34b862fdda0680e78fa2bdb86c5cba613bee1
federicogodoy <federicogodoy@icloud.com>
2017-06-07 11:39:50 -0300

    Add files via upload
    File handler v1.0 (chequear? que es eso)

---
+++ FileHandler/FileHandler.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+
+static double info[MAX_AMUNT_OF_DATA];
+
+int16_t F_Startup (char * filename)
+{
+	uint16_t counter = 0;
+	FILE *fp;
+	fp = fopen(filename,"r");
+	if(fp)
+	{
+		while(fscanf(fp,"%lf",info[counter++]) != 0)
+		{
+		}
+
+	}
+	else
+		return(F_ERROR_CANTOPEN);
+	fclose(fp);
+	return(F_OK);
+
+}
+
+uint16_t F_getBasicInfo (uint16_t order)
+{
+	if(order<11)
+	{
+		return F_getInfo(order);
+	}
+	else
+		return F_ERROR_WAYD;
+}
+
+
+double F_getSensorType (uint16_t sensornumber)
+{
+	return F_getInfo((sensornumber*4)+10);
+}
+
+double F_getSensorXPos (uint16_t sensornumber)
+{
+	return F_getInfo((sensornumber*4)+11);
+}
+
+double F_getSensorYPos (uint16_t sensornumber)
+{
+	return F_getInfo((sensornumber*4)+12);
+}
+
+double F_getSensorAngle (uint16_t sensornumber)
+{
+	return F_getInfo((sensornumber*4)+13);
+}
+
+static double F_getInfo (uint16_t order)
+{
+	if(order > MAX_AMUNT_OF_DATA)
+		return F_ERROR_WAYD;
+	return info[order];
+}
+
