91406fc0921f814b8960f199b7ab200d733e1c08
rocioparra <rparra@itba.edu.ar>
2017-06-30 09:37:52 -0300

    main respeta estructura de archivos, vector double
    agregue dpoint y dvector, con tipo de dato double

--- vector.h
+++ vector.h
@@ -1,31 +1,29 @@
 #ifndef VECTOR_H
 #define	VECTOR_H
 
+#ifdef __cplusplus
+#include <cstdint>
+#else
 #include <stdint.h>
-
+#endif // __cplusplus
 
 //tipos de datos punto (dos coordenadas cartesianas) y vector (dos puntos) con distintos tipos de datos
-
 //ESTO ES UN BORRADOR MALE NO ME RETES
 
-typedef struct { float x, y;} fpoint_t;
+//con double
+typedef struct { double x, y; } dpoint_t;
+typedef struct { dpoint_t start, end; } dvector_t;
 
-typedef struct { fpoint_t start, end; } fvector_t;
+//con float
+typedef struct { float x, y;} fdpoint_t;
+typedef struct { fdpoint_t start, end; } fvector_t;
 
+//con unsigned int de 16 bits
+typedef struct { uint16_t x, y;} uidpoint_t;
+typedef struct { uidpoint_t start, end; } uivector_t;
 
-typedef struct { uint16_t x, y;} uipoint_t;
-
-typedef struct { uipoint_t start, end; } uivector_t;
-
-typedef struct { int16_t x, y;} ipoint_t;
-
-typedef struct { ipoint_t start, end; } ivector_t;
-
-typedef struct
-{
-	double x;
-	double y;
-}Point_t;
-
+//con signed int de 16 bits
+typedef struct { int16_t x, y;} idpoint_t;
+typedef struct { idpoint_t start, end; } ivector_t;
 
 #endif //VECTOR_H
