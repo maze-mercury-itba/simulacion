8b617daacfceb9b62402557be532a99bdfb34aa7
rocioparra <rparra@itba.edu.ar>
2017-08-26 11:52:29 -0300

    borro carpeta de mas

--- Inteligencia/Intelligence/Autos/IntelligenceCalculus.h
+++
@@ -1,9 +0,0 @@
-#ifndef INTELLIGENCECALCULUS
-#define INTELLIGENCECALCULUS
-void actOverInclination(double inc);
-void goBack(void);
-void turnRight(void);
-void turnLeft(void);
-void moveFoward(void);
-void goSlightlyRight(void);
-#endif //INTELLIGENCECALCULUS
